3b8c7778ec108c9c557703f98e3954867a583f79
hellobrother1472 <paras1472@gmail.com>
2022-10-31 12:56:33 +0530

    new

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get("/contact", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
     const path = require("path");
-    app.get("*", (req, res) => {
+    app.get("/", (req, res) => {
         app.use(express.static(path.resolve(__dirname, 'client', 'build')));
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
